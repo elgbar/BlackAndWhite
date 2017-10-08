a76fea8ae2b597cd70b42a56467aa8dffdc2aebc
Karl Henrik Elg <klink100@gmail.com>
2017-10-08 15:57:37 +0200

    Create template

---
+++ desktop/src/no/kh498/bnw/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package no.kh498.bnw.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import no.kh498.bnw.BlackAndWhite;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BlackAndWhite(), config);
+	}
+}
